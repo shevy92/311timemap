7c861d6629465a93a57d3af3b7b86f63a25bf21c
shevy92 <Shevy@Sevikas-MacBook-Pro-2.local>
2015-11-08 16:45:43 -0500

    Selection by year is functioning

--- dates.js
+++ dates.js
@@ -6,24 +6,35 @@
 * Visit JavaScript Kit at http://www.javascriptkit.com/ for this script and more
 ***********************************************/
 
-var monthtext=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
+var monthtext=['ALL','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
 
 function populatedropdown(dayfield, monthfield, yearfield){
-var today=new Date()
-var dayfield=document.getElementById(dayfield)
-var monthfield=document.getElementById(monthfield)
-var yearfield=document.getElementById(yearfield)
-for (var i=0; i<31; i++)
-dayfield.options[i]=new Option(i, i+1)
-dayfield.options[today.getDate()]=new Option(today.getDate(), today.getDate(), true, true) //select today's day
-for (var m=0; m<12; m++)
-monthfield.options[m]=new Option(monthtext[m], monthtext[m])
-monthfield.options[today.getMonth()]=new Option(monthtext[today.getMonth()], monthtext[today.getMonth()], true, true) //select today's month
-var thisyear=today.getFullYear()
-for (var y=0; y<20; y++){
-yearfield.options[y]=new Option(thisyear, thisyear)
-thisyear+=1
-}
-yearfield.options[0]=new Option(today.getFullYear(), today.getFullYear(), true, true) //select today's year
+    var today=new Date()
+    var dayfield=document.getElementById(dayfield)
+    var monthfield=document.getElementById(monthfield)
+    var yearfield=document.getElementById(yearfield)
+
+    //populate day
+    dayfield.options[0]=new Option('ALL');
+
+    for (var i=1; i<32; i++) {
+        dayfield.options[i]=new Option(i, i)
+    }
+
+    //populate month
+    for (var m=0; m<13; m++) {
+        monthfield.options[m]=new Option(monthtext[m], monthtext[m])
+    }
+
+    var thisyear=today.getFullYear()
+
+    //populate for past 5 years
+    yearfield.options[0]=new Option('ALL');
+    
+    for (var y=1; y<6; y++){
+        yearfield.options[y]=new Option(thisyear, thisyear)
+        thisyear-=1
+    }
+ 
 }
 
